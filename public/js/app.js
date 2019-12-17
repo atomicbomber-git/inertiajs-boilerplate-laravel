ce9d55358e1b43f32b875ac4aa3130635a164d51
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-12-17 10:23:13 +0700

    Set-up a readme page.

--- public/js/app.js
+++ public/js/app.js
@@ -51068,8 +51068,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/atomicbomber/projects/james-erp/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/atomicbomber/projects/james-erp/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/atomicbomber/projects/inertiajs-boilerplate-laravel/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/atomicbomber/projects/inertiajs-boilerplate-laravel/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
